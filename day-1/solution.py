b8d5b9a6c9f4dd4970711a1c4dcbc43a00951d06
Ho Seok Lee <davhslee@gmail.com>
2021-12-16 09:09:42 +0000

    Completed day 1.

---
+++ day-1/solution.py
@@ -0,0 +1,60 @@
+import numpy as np
+
+
+
+'''
+    PROCESS INPUTS
+'''
+
+# Open and parse input text
+with open("measurements.txt", "r") as f:
+    raw_lines = f.read()
+
+# Convert to integers
+measurements = list(map(
+    int,
+    
+    raw_lines.split("\n")
+))
+
+
+
+'''
+    PART ONE: PERFORM COUNT
+
+    Complexity: O(n)
+    
+    Simply compare the current number and the next to count the number of increases.
+'''
+'''
+increases = sum([
+    int(measurements[i] < measurements[i + 1])
+
+    for i in range(len(measurements) - 1)
+])
+
+print(increases)
+'''
+
+
+
+'''
+    PART TWO: CONVOLVE AND PERFORM COUNT
+
+    Complexity: O(n * m)
+    
+    Convolve first and then perform the operation.
+'''
+convolved = np.convolve(
+    np.array(measurements), 
+    np.ones(3), 
+    mode='valid'
+)
+
+increases = sum([
+    int(convolved[i] < convolved[i + 1])
+
+    for i in range(len(convolved) - 1)
+])
+
+print(increases)
